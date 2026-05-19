591e5fc3b32fb1528935437bfd31f297a3b73adb
Pierre Chamberlain <chamberlainpi@gmail.com>
2026-05-19 20:16:29 -0300

    feat: red koopa started (not fully finished yet)

---
+++ Scripts/Enemies/RedKoopa.cs
@@ -0,0 +1,17 @@
+class RedKoopa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("This is the Red Koopa!");
+    }
+
+    public void Move()
+    {
+        Console.WriteLine("The Red Koopa is moving!");
+    }
+
+    public void Attack()
+    {
+        Console.WriteLine("The Red Koopa is attacking!");
+    }
+}
